abad810b195fb28b08fdfd357cf1ee7f59f3c991
Aleksi Tappura <atappura@gmail.com>
2020-03-14 10:14:26 +0200

    fix fonts

--- .eleventy.js
+++ .eleventy.js
@@ -90,6 +90,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
+  eleventyConfig.addPassthroughCopy("static/fonts");
+
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
 
